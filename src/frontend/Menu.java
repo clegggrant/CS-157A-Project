224f75965626728bf9a1de950c22a70786fd0766
Monty Saengsavang <montysaengsavang@Montys-MacBook-Pro-2.local>
2018-11-21 13:16:11 -0800

    Created menu and functions template Menu is setup, just need to do some backend work

--- src/frontend/Menu.java
+++ src/frontend/Menu.java
@@ -1,9 +1,17 @@
 package frontend;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Represents the interface the user will use when running
@@ -12,7 +20,7 @@ import java.awt.GridLayout;
  *
  */
 
-public class Menu {
+public class Menu extends JFrame{
 	
 	public final Font font = new Font("Arial", Font.PLAIN, 20);
 	
@@ -28,8 +36,8 @@ public class Menu {
 	 */
 	private JFrame initializeJF(){
 		JFrame output = new JFrame("Bank Management System");
-		output.setSize(1500,500);
-		output.setLayout(new GridLayout(4,4));
+		output.setSize(300,130);
+	
 		output.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		return output;
 	}
@@ -41,55 +49,52 @@ public class Menu {
 	 * @param jf The JFrame to add the buttons to
 	 */
 	private void addButtons(JFrame jf){
-		JButton belowMinBalance = new JButton("View Savings Below Min Balance");
-		jf.add(belowMinBalance);
 		
-		JButton allBalances = new JButton("View All Balances");
-		//allBalances.setFont(font);
+		//create panel to hold all buttons in flow layout
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(new FlowLayout());
+		
+		
+		//create empManag button
+		JButton employeeManagement = new JButton("Employee Management");
+		buttonPanel.add(employeeManagement);
+		//create action listener for the empManag button
+		employeeManagement.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// dispose of the current frame
+				jf.dispose();
 
-		jf.add(allBalances);
+				//call the employee management frame
+				JFrame empManagement = new EmployeeFrame();
+			}
+		});
 		
-		JButton allEmpsWithAccts = new JButton("List Employees with Accounts");
-		jf.add(allEmpsWithAccts);
 		
-		JButton avgSalary = new JButton("View Above Avg Salary");
-		jf.add(avgSalary);
+		JButton transactionInfo = new JButton("Transaction Information");
+		buttonPanel.add(transactionInfo);
+		transactionInfo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// dispose of the current frame
+				jf.dispose();
+
+				//call the employee management frame
+				JFrame transacInfo = new TransactionFrame();
+			}
+		});
 		
-		JButton belowAvgSalary = new JButton("View Below Avg Salary");
-		jf.add(belowAvgSalary);
+
+		JButton accountInfo = new JButton("Account Information");
+		buttonPanel.add(accountInfo);
+		accountInfo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// dispose of the current frame
+				jf.dispose();
+
+				//call the employee management frame
+				JFrame accountInfo = new AccountFrame();
+			}
+		});
 		
-		JButton loansWithDebt = new JButton("View Loans that Owe X");
-		jf.add(loansWithDebt);
-		
-		JButton lessThanMinPayment = new JButton("View Payments Below Min");
-		jf.add(lessThanMinPayment);
-		
-		JButton purchaseAboveLimit = new JButton("View Purchaces Above Limit");
-		jf.add(purchaseAboveLimit);
-		
-		JButton loansEndingInYear = new JButton("View Loans Ending Soon");
-		jf.add(loansEndingInYear);
-		
-		JButton profitFromCreditInterest = new JButton("View Credit Card Profit");
-		jf.add(profitFromCreditInterest);
-		
-		JButton savingsInterestExpense = new JButton("View Savings Interest Expense");
-		jf.add(savingsInterestExpense);
-		
-		JButton employeeExpense = new JButton("View Payroll Expense");
-		jf.add(employeeExpense);
-		
-		JButton viewDefaultRisks = new JButton("View Potential Loan Defaults");
-		jf.add(viewDefaultRisks);
-		
-		JButton calcLoanProfit = new JButton("Calculate Loan Profit");
-		jf.add(calcLoanProfit);
-		
-		JButton hire = new JButton("Hire Employee");
-		jf.add(hire);
-		
-		JButton fire = new JButton("Fire Employee");
-		jf.add(fire);
 		
 		for(int i = 0; i < jf.getContentPane().getComponentCount(); i++){
 			
@@ -98,5 +103,6 @@ public class Menu {
 			}
 		}
 		
+		jf.add(buttonPanel);
 	}
 }
