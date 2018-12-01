ff89821b2b844e5bae2b89e2b9c66e4e3f2725fd
Yu Jun Zhao <yuj.zhao4@gmail.com>
2018-12-01 00:39:22 -0800

    Changed panel switching code

--- src/frontend/Menu.java
+++ src/frontend/Menu.java
@@ -6,79 +6,88 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Menu {
+	public static final Dimension WINDIMENSION = Toolkit.getDefaultToolkit().getScreenSize();
+	public static final String MENU = "MENU";
+	public static final String EMPANEL = "EMPLOYEE PANEL";
 	
 	private JFrame jf = new JFrame("Bank Management System");
+	private JPanel cardPanel = new JPanel(new CardLayout());; // for switching panels.
 	
+
 	public Menu() {
-		initialize(jf);
-		addButtons(jf);
+		
+		initialize();
+		
+		cardPanel.add(getMenuPanel(), MENU);		
+		cardPanel.add(new EmployeePanel(), EMPANEL);
+		jf.add(cardPanel, BorderLayout.CENTER);
+		
 		jf.setVisible(true);
 	}
 	
-	private void initialize(JFrame jf) {
+	private void initialize() {
 		jf.setLayout(new BorderLayout());
-		
-		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		int height = (int)(screenSize.getHeight()/4);
-		int width = (int)(screenSize.getWidth()/4);
+
+		int height = (int)(WINDIMENSION.getHeight()/4);
+		int width = (int)(WINDIMENSION.getWidth()/4);
 		jf.setSize(width, height);
 		
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		jf.setLocationRelativeTo(null);
 	}
 	
-	private void addButtons(JFrame jf) {
-		JPanel buttonPanel = new JPanel();
-		buttonPanel.setLayout(new GridLayout(3,1,20,20));
-		buttonPanel.setBorder(new EmptyBorder(20,20,20,20));
+	private JPanel getMenuPanel() {
+		
+		JPanel menuPanel = new JPanel();
+		menuPanel.setLayout(new GridLayout(3,1,20,20));
+		menuPanel.setBorder(new EmptyBorder(20,20,20,20));
 		
 		//create empManag button
-				JButton employeeManagement = new JButton("Employee Management");
-				employeeManagement.setAlignmentX(Component.CENTER_ALIGNMENT);
-				employeeManagement.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent e) {
-						JPanel employeePanel = new EmployeePanel();
-						jf.add(employeePanel, BorderLayout.CENTER);
-						buttonPanel.setVisible(false);
-						//jf.remove(buttonPanel);
-						Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-						int height = (int)(screenSize.getHeight()/3);
-						int width = (int)(screenSize.getWidth()/3);
-						jf.setSize(width, height);
-						jf.validate();
-						jf.repaint();
-					}
-				});
-				
-				
-				JButton transactionInfo = new JButton("Transaction Information");
-				transactionInfo.setAlignmentX(Component.CENTER_ALIGNMENT);
-				transactionInfo.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent e) {
-						JOptionPane.showMessageDialog(jf, "Feature not yet implemented!", "Error", JOptionPane.ERROR_MESSAGE);
-					}
-				});
-
-				
-				JButton accountInfo = new JButton("Account Information");
-				accountInfo.setAlignmentX(Component.CENTER_ALIGNMENT);
-				accountInfo.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent e) {
-						JOptionPane.showMessageDialog(jf, "Feature not yet implemented!", "Error", JOptionPane.ERROR_MESSAGE);
-					}
-				});
-				
-				buttonPanel.add(employeeManagement);
-				buttonPanel.add(transactionInfo);
-				buttonPanel.add(accountInfo);
-				
-				for(int i = 0; i < buttonPanel.getComponentCount(); i++){
+			JButton employeeManagement = new JButton("Employee Management");
+			employeeManagement.setAlignmentX(Component.CENTER_ALIGNMENT);
+			employeeManagement.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
 					
-					if(buttonPanel.getComponents()[i] instanceof JButton){
-						buttonPanel.getComponents()[i].setFont(new Font("Arial", Font.PLAIN, 30));
-					}
+					int height = (int)(WINDIMENSION.getHeight()/3);
+					int width = (int)(WINDIMENSION.getWidth()/3);
+					jf.setSize(width, height);
+					jf.setLocationRelativeTo(null);
+
+					CardLayout c = (CardLayout)cardPanel.getLayout();
+					c.show(cardPanel, EMPANEL);
+										
 				}
+			});
+			
+			
+			JButton transactionInfo = new JButton("Transaction Information");
+			transactionInfo.setAlignmentX(Component.CENTER_ALIGNMENT);
+			transactionInfo.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					JOptionPane.showMessageDialog(jf, "Feature not yet implemented!", "Error", JOptionPane.ERROR_MESSAGE);
+				}
+			});
+
+			
+			JButton accountInfo = new JButton("Account Information");
+			accountInfo.setAlignmentX(Component.CENTER_ALIGNMENT);
+			accountInfo.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					JOptionPane.showMessageDialog(jf, "Feature not yet implemented!", "Error", JOptionPane.ERROR_MESSAGE);
+				}
+			});
+			
+			menuPanel.add(employeeManagement);
+			menuPanel.add(transactionInfo);
+			menuPanel.add(accountInfo);
+			
+			for(int i = 0; i < menuPanel.getComponentCount(); i++){
 				
-				jf.add(buttonPanel, BorderLayout.CENTER);
+				if(menuPanel.getComponents()[i] instanceof JButton){
+					menuPanel.getComponents()[i].setFont(new Font("Arial", Font.PLAIN, 30));
+				}
+			}
+			
+			return menuPanel;
 	}
 }
